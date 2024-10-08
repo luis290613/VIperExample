616d9420d54cada3dcf1d4170498f51aa278b745
Luis Diego Ruiz Bautista <lruiz@tribalworldwide.gt>
2024-10-08 08:12:42 -0500

    Configuración para que el proyecto no use Storyboard

--- VIperExample/SceneDelegate.swift
+++ VIperExample/SceneDelegate.swift
@@ -4,19 +4,27 @@
 //
 //  Created by Diego on 8/10/24.
 //
+// MARK: Project setup without storyboard :
+// https://www.youtube.com/watch?v=ZMbHkMutaMk&t=321s&ab_channel=SwiftBeta
+
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        let controller = ViewController()
+        controller.title = "Welcome to future"
+        
+        let navigationController = UINavigationController(rootViewController: controller)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
